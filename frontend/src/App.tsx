68dfeeac7c5ae932a9866cea3af3fdd0d3b0a77a
Gjenio540 <pawelspisz@gmail.com>
2023-05-18 12:21:21 +0200

    Vite Configuration
    - Vite configuration
    - Types for NodeMailer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import './App.scss'
 import MainPage from './pages/MainPage'
+import SignInPage from './pages/SignInPage'
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<MainPage/>}/>
+        <Route path='logowanie' element={<SignInPage/>}/>
       </Routes>
     </BrowserRouter>
   )
