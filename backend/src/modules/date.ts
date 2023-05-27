98a100a7951d6ba02845836115912712a0e2854f
Gjenio540 <pawelspisz@gmail.com>
2023-05-27 14:19:41 +0200

    Stashed changes

--- backend/src/modules/date.ts
+++ backend/src/modules/date.ts
@@ -1,12 +1,3 @@
-<<<<<<< Updated upstream
-export function getDateISOString(): string {
-    let date = new Date()
-    let currentDate = date.toISOString()
-    return currentDate
-}
-
-=======
->>>>>>> Stashed changes
 export function getDateString(): string {
     let date = new Date()
     let currentDate = date.toLocaleDateString("pl-PL")
